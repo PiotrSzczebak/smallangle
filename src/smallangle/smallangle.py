7756ef8366184b520bb56aa7af80dab407add691
PiotrSzczebak <p.szczebak10@gmail.com>
2024-11-15 14:44:49 +0100

    Smallangle exercise
    Installing, changing and adding docstrings to smallangle,py.

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,20 +1,53 @@
 import click
 import numpy as np
-from numpy import pi
 import pandas as pd
+from numpy import pi
 
 
+@click.group()
+def calc_group():
+    pass
+
+
+@calc_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    type=int,
+    help="number of x values between o and 2pi",
+    show_default=True,
+)
 def sin(number):
+    """Gives a list of NUMBER values for x and tan(x) between 0 and 2pi.
+
+
+    NUMBER is the number of point between 0 and 2 pi.
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
 
+@calc_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    type=int,
+    help="number of x values between o and 2pi",
+    show_default=True,
+)
 def tan(number):
+    """Gives a list of NUMBER values for x and tan(x) between 0 and 2pi.
+
+
+    NUMBER is the number of point between 0 and 2 pi.
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
 
 if __name__ == "__main__":
-    sin(10)
+    calc_group()
